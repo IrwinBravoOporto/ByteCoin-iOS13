392539a501ba93f5138821312c5481ba09ffb2a4
irwin <irwinbravooporto@gmail.com>
2020-11-24 18:57:41 -0500

    finish

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by everis on 11/24/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData:Codable {
+    let rate:Double
+}
